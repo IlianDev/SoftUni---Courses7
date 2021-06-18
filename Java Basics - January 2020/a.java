f5e2c597a2c562c5cf240e4b7039b813b781707d
Ilian Kostov <iliankostov@Ilians-MacBook-Pro.local>
2021-06-18 16:00:21 +0300

    Edit

--- Java Basics - January 2020/a.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class a {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = Integer.parseInt(scanner.nextLine());
-        
-    }
-}
