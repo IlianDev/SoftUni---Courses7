5a4bb5554b6bce5562cfe9cc15d2a125c1b3bc93
Ilian Kostov <63207846+IlianDev@users.noreply.github.com>
2021-06-23 17:47:53 +0300

    Add files via upload

---
+++ Python Advanced - May 2021/Modules/authenticator.py
@@ -0,0 +1,8 @@
+from gui_shop_live_demo.canvas import tk
+from tkinter import Button
+
+
+def render_main_enter_screen():
+    Button(tk, text="Login", bg="green", fg="black").grid(row=0, column=0)
+    Button(tk, text="Registration", bg="yellow", fg="black").grid(row=0, column=1)
+
