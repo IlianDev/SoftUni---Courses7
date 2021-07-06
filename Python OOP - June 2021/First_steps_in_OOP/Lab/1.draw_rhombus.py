267f501f30f3737424a1653d14af52546a5f726e
Ilian Kostov <iliankostov@Ilians-MacBook-Pro.local>
2021-07-06 17:10:01 +0300

    Lab and Exercises

---
+++ Python OOP - June 2021/First_steps_in_OOP/Lab/1.draw_rhombus.py
@@ -0,0 +1,17 @@
+def draw_rhombus(n):
+    for i in range(n):
+        offset = (n - i - 1) * " "
+        content = ("* " * (i + 1)).strip()
+        print(f"{offset}{content}")
+
+    for i in range(n - 2, -1, -1):
+        offset = (n - i - 1) * " "
+        content = ("* " * (i + 1)).strip()
+        print(f"{offset}{content}")
+
+
+draw_rhombus(5)
+draw_rhombus(4)
+draw_rhombus(3)
+draw_rhombus(2)
+draw_rhombus(1)
