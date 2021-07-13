46760e1db46e7c7ab34fcbda23223b0eb2bdc062
Ilian Kostov <iliankostov@Ilians-MacBook-Pro.local>
2021-07-13 16:12:35 +0300

    lab

--- Python OOP - June 2021/Inheritence/Lab/01. Food/project/fruit.py
+++
@@ -1,9 +0,0 @@
-from project_food.food import Food
-
-
-class Fruit(Food):
-    def __init__(self, name=str, expiration_date=str):
-        super().__init__(expiration_date)
-        self.name = name
-
-# kato dadem na papkata Inheritance_lab_ex desen buton mark as root и импортжаме после само от нея по бързо достъпване
