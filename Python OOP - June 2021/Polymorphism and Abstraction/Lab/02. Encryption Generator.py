ea2661586a06e33aa7a444fb289b1b39af78e80d
Ilian Kostov <iliankostov@Ilians-MacBook-Pro.local>
2021-08-01 08:43:22 +0300

    OOP

---
+++ Python OOP - June 2021/Polymorphism and Abstraction/Lab/02. Encryption Generator.py
@@ -0,0 +1,29 @@
+class EncryptionGenerator:
+    def __init__(self, text):
+        self.text = text
+
+    def __add__(self, other):
+        if not isinstance(other, int):  # ако не си число
+            raise ValueError("You must add a number.")
+
+        result = ""
+
+        for char in self.text:
+            encoded_char = ord(char) + other
+            while encoded_char < 32:
+                encoded_char += 95  # 95 = 126 - 32
+            while encoded_char > 126:
+                encoded_char -= 95
+            result += chr(encoded_char)
+        return result
+
+
+# 1
+some_text = EncryptionGenerator('I Love Python!')
+print(some_text + 1)
+print(some_text + (-1))
+
+# 2
+example = EncryptionGenerator('Super-Secret Message')
+print(example + 20)
+print(example + (-52))
