942f67e8bba5b4c4b5a20d6f09273d17e0ce8539
IlianDev <ilian.g.kostov@gmail.com>
2021-08-10 13:36:51 +0300

    OOP

---
+++ Python OOP - June 2021/Decorators/Lab/02. Vowel Filter.py
@@ -0,0 +1,14 @@
+def vowel_filter(function):
+    def wrapper():
+        result = function()
+        return [letter for letter in result if letter.lower() in "aeuio"]
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+
+print(get_letters())
