9ddd65450cef5fec3c6c1faee1274bd43eada040
IlianDev <ilian.g.kostov@gmail.com>
2021-08-19 16:07:53 +0300

    OOP ex5

--- Python OOP - June 2021/Inheritence/Exercise/05. Shop/project/product_repository.py
+++ Python OOP - June 2021/Inheritence/Exercise/05. Shop/project/product_repository.py
@@ -8,13 +8,15 @@ class ProductRepository:
         self.products.append(product.name)
 
     def find(self, product_name):
-        return f"{product_name.name}"
+        for prod in self.products:
+            if prod.name == product_name:
+                return prod
 
     def remove(self, product_name):
-        ProductRepository.products.remove(product_name.name)
+        for prod in self.products:
+            if prod.name == product_name:
+                self.products.remove(prod)
 
     def __repr__(self):
         for i in ProductRepository.products:
             return f"{i}: {Product.__name__}"
-
-
