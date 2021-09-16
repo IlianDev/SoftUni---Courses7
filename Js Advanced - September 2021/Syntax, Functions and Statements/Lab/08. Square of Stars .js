e728bf75c37d1a6fb8722ee1aad2d02cfe80aa6f
Ilian Kostov <ilian.g.kostov@gmail.com>
2021-09-16 16:36:09 +0300

    js adv

---
+++ Js Advanced - September 2021/Syntax, Functions and Statements/Lab/08. Square of Stars .js
@@ -0,0 +1,11 @@
+function solve(num) {
+    let ll = []
+    for (let temp = 0; temp < num; temp++) {
+        ll.push('*')
+    }
+
+    for (let i = 0; i < num; i++) {
+        console.log(ll.join(' '));
+    }
+}
+solve(5)
