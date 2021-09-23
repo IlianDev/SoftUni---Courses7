8aa3438f91da3cd322f035ff1db75b17c33a7f05
Ilian Kostov <ilian.g.kostov@gmail.com>
2021-09-23 16:47:02 +0300

    js

---
+++ Js Advanced - September 2021/Arrays/Lab/basicSintax/splice.js
@@ -0,0 +1,5 @@
+names = ['Gosho', 'Pesho', 'Stamat', 'Mariyka']
+// let removeResutlt = names.splice(2, 1);
+let addResult = names.splice(2,0, 'Dragan')
+// console.log(names); // (3) ['Gosho', 'Pesho', 'Mariyka']
+console.log(names);
