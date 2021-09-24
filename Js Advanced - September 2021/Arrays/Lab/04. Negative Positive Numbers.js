453a48c504a0b792c42a564f0139965d3c4c91d9
Ilian Kostov <ilian.g.kostov@gmail.com>
2021-09-24 15:16:35 +0300

    correct

--- Js Advanced - September 2021/Arrays/Lab/04. Negative Positive Numbers.js
+++ Js Advanced - September 2021/Arrays/Lab/04. Negative Positive Numbers.js
@@ -1,9 +1,18 @@
-function solve(arr) {
-    arr.sort(sortingEl)
-    console.log(arr)
+function solve(numbers){
+    const result = [];
 
-    function sortingEl(a, b) {
-        return a - b
+    for (let num of numbers) {
+        if (num < 0){
+            result.unshift(num);
+        }else{
+            result.push(num);
+        }
+    }
+    for (let i of result) {
+        console.log(i);
     }
 }
 solve([7, -2, 8, 9])
+console.log('---')
+solve([3, -2, 0, -1])
+
