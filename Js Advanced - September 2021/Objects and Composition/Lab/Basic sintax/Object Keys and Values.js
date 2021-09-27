7442ab1e91eb32adfa56e9b8273fed134c9522a7
Ilian Kostov <ilian.g.kostov@gmail.com>
2021-09-28 00:44:15 +0300

    js adv

---
+++ Js Advanced - September 2021/Objects and Composition/Lab/Basic sintax/Object Keys and Values.js
@@ -0,0 +1,6 @@
+const phonebook = { 'Tim': '555-111',
+                    'Bill': '555-333',
+                    'Peter': '555-777' };
+
+const keys = Object.values(phonebook);
+console.log(keys);
