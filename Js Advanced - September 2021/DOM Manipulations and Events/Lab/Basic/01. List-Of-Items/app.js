cd53b512ec93e6237a16bde8780678f30b84bf49
Ilian Kostov <ilian.g.kostov@gmail.com>
2021-10-18 17:01:12 +0300

    js dom lab

---
+++ Js Advanced - September 2021/DOM Manipulations and Events/Lab/Basic/01. List-Of-Items/app.js
@@ -0,0 +1,13 @@
+function addItem() {
+    // select input field end read value
+    const input = document.getElementById('newItemText');
+
+    // create new <li> and set its content ot input value
+    const liElement = document.createElement('li');
+    liElement.textContent = input.value;
+    // selct <ul> and append new <li> element 
+    document.getElementById('items').appendChild(liElement);
+    // nice-to-have clear field
+    input.value = '';
+
+}
